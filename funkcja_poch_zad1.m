0b9265edf20165672ac20bdd463dd83895f81edd
Bartosz Rajkowski <bartosz.rajkowski@gmail.com>
2016-05-15 01:32:06 +0200

    algorytmy do pierwszego zad

---
+++ funkcja_poch_zad1.m
@@ -0,0 +1,7 @@
+function [ y ] = funkcja_poch_zad1( x )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+y=5*exp(-x)-2;
+
+end
+
---
+++ funkcja_zad1.m
@@ -0,0 +1,5 @@
+function [ y ] = funkcja_zad1( x )
+%obliczanie wartosci funkcji y=7.2-2x-5e^(-x)
+y=7.2-2*x-5*exp(-x);
+end
+
